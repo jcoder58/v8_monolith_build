fc93b7f2e12caeb66ac90699f1dd7cb6da642419
jcoder58 <whitnell@gmail.com>
2023-12-12 11:11:14 -0500

    Update bump.sh

--- bump.sh
+++ bump.sh
@@ -3,5 +3,5 @@ RELEASE=12.0
 git tag --delete $RELEASE
 git push --delete origin $RELEASE
 git commit -a -m "v8 release $RELEASE"
-git tag $RELEASE
+git tag $RELEASE 
 git push origin main --force --tags
